10d5a4b475496c9e22d813ad0822dc98be16f095
Mohamed _Saleh <127530951+mohamedsalehosman1@users.noreply.github.com>
2025-12-01 12:13:09 +0200

    first commit

--- Modules/Products/Resources/views/products/sidebar.blade.php
+++ Modules/Products/Resources/views/products/sidebar.blade.php
@@ -5,25 +5,24 @@
     @slot('isActive', request()->routeIs('*products*'))
     @slot('icon', 'fas fa-tshirt')
     @slot('tree', [
-
-[dd('hello')],[
+        [
         'name' => trans('products::products.actions.list'),
         'url' => route('dashboard.products.index'),
-        // 'can' => ['permission' => 'read_products'],
+        'can' => ['permission' => 'read_products'],
         'isActive' => request()->routeIs('*products.index'),
         'module' => 'Products',
         ],
         [
         'name' => trans('products::products.actions.create'),
         'url' => route('dashboard.products.create'),
-        // 'can' => ['permission' => 'create_products'],
+        'can' => ['permission' => 'create_products'],
         'isActive' => request()->routeIs('*products.create'),
         'module' => 'Products',
         ],
         [
         'name' => trans('products::products.actions.pending_requests'),
         'url' => route('requests'),
-        // 'can' => ['permission' => 'read_products'],
+        'can' => ['permission' => 'read_products'],
 'badge' => auth()->user()->can('read_products')
     ? \Modules\Products\Entities\Product::where('status', 'pending')->count()
     : 0,
